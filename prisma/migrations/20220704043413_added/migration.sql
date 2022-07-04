59636f1ae3622c17d3adc523ba6d07da8e7d127e
invalidcode232 <invalidcode69@gmail.com>
2022-07-04 12:05:25 +0700

    Used next-auth as authentication

---
+++ prisma/migrations/20220704043413_added/migration.sql
@@ -0,0 +1,95 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `user_id` on the `flashcard_history` table. All the data in the column will be lost.
+  - The primary key for the `users` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `password` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `registered_at` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `username` on the `users` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[email]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `userId` to the `flashcard_history` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "flashcard_history" DROP CONSTRAINT "flashcard_history_user_id_fkey";
+
+-- AlterTable
+ALTER TABLE "flashcard_history" DROP COLUMN "user_id",
+ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "flashcards" ALTER COLUMN "question" SET DATA TYPE VARCHAR(300);
+
+-- AlterTable
+ALTER TABLE "users" DROP CONSTRAINT "users_pkey",
+DROP COLUMN "password",
+DROP COLUMN "registered_at",
+DROP COLUMN "username",
+ADD COLUMN     "email_verified" TIMESTAMP(3),
+ADD COLUMN     "image" TEXT,
+ADD COLUMN     "name" TEXT,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "email" DROP NOT NULL,
+ALTER COLUMN "email" SET DATA TYPE TEXT,
+ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "users_id_seq";
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "verificationtokens" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verificationtokens_token_key" ON "verificationtokens"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verificationtokens_identifier_token_key" ON "verificationtokens"("identifier", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "flashcard_history" ADD CONSTRAINT "flashcard_history_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
