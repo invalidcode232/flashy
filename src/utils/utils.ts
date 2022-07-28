0074752ed6541d21225c58509e5a255fb36f0427
invalidcode232 <invalidcode69@gmail.com>
2022-07-28 18:05:49 +0700

    refactor: removed lodash for camelcase-keys

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,5 @@
-import * as _ from 'lodash';
-
 const capitalizeString = (str: string) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
-const camelize = (obj: any) => {
-    return _.mapKeys(obj, (v, k) => _.camelCase(k));
-};
-
-export { capitalizeString, camelize };
+export { capitalizeString };
